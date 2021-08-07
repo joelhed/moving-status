38fcf6fb9d3befe4d3c29ad13c69087ea561b5a5
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2021-08-07 14:37:12 +0200

    Initial Commit

---
+++ Moving Status/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Moving Status
+//
+//  Created by Mac on 2021-08-07.
+//  Copyright © 2021 Joel Hedtjärn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
